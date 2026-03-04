2518f51e6fd85401ec2ff0373dfae47ce6e1db36
Andre <dremartinss07@gmail.com>
2026-03-04 16:44:48 +0000

    foundation for rest

---
+++ horseRace.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+void advance(int horsENum, int* horses);
+void printLane(int horseNum, int* horses);
+bool isWinner(int horseNum, int* horses);
+
+const int NUM_HORSES = 5;
+const int TRACK_LENGTH = 15;
+
+int main(){
+	std::cout << "Horse race game " << std:: endl;
+	srand(time(NULL));
+
+	int horses[NUM_HORSES] = {0, 0, 0, 0, 0};
+
+	// test
+	for (int i = 0; i < 10; i++){
+		advance(0, horses);
+		printLane(0, horses);
+	} // end for
+
+
+	return 0;
+} // end main
+
+void printLane(int horseNum, int* horses){
+	for (int i = 0; i < TRACK_LENGTH; i++){
+		if (i == horses[horseNum]){
+			std::cout << horseNum;
+		} else {
+			std::cout << ".";
+		} // end if
+	} // end for loop
+	// print newLine
+	std::cout << std::endl;
+} // end printLane
+
+void advance(int horseNum, int* horses){
+	int coin = rand() % 2;
+	horses[horseNum] += coin;
+} // end advance
+

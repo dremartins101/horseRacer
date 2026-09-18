05f39ac5ba34feea2dfecd3315b11d26d2a96865
Andre <dremartinss07@gmail.com>
2026-09-18 16:49:37 +0000

    have to finish main

--- horseRace.cpp
+++ horseRace.cpp
@@ -16,29 +16,32 @@ int main(){
 	bool keepGoing = true
 
 	while (winner = true){
-
+	for (int
 	}
-
-
-
-
 return 0
 } // end main
  
-
 void advance(int horseNum, int* horses){
 	int coinFlip = rand() % 1;
 	horses[horseNum] += coinFlip;
-}
-
+} // end advance
 
 void printLane(int horseNum, int* horses){
 	for(int i = 0; i < TRACK_LENGTH; i++){
-		
+	if (i == horses[horseNum]){
+		std::cout << horses[horseNum] << std::endl;	
+	} else {
+		std::cout << "." << std::endl;
 	}
 
-}
-
+	std::cout << "\n" << std::endl;
+} // end printLane
 bool isWinner(int horseNum, int* horses){
-
+	if(horses[horseNum] == TRACK_LENGTH){
+		std::cout << horseNum << std::endl;
+		return true;
+	} // end if
+	else {
+	return false;
+	} // end else	
 }
